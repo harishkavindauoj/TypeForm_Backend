fea77170f8221533b53c4d6155a37afb2cb03b88
Harish_Kavinda <2018ict35@vau.jfn.ac.lk>
2024-06-16 00:48:31 +0530

    update: Mongo cloud database

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb://localhost:27017")
+MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb+srv://harishscc123:S97r3Gdja1JA60PH@forms.ejlqcyb.mongodb.net/?retryWrites=true&w=majority&appName=Forms")
+
 client = AsyncIOMotorClient(MONGO_DETAILS)
+
 database = client.typeform
 forms_collection = database.get_collection("forms")
 
