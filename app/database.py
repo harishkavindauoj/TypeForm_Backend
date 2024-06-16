5a659f458341b96d0dde404fcf93fdbfd7080819
Harish_Kavinda <2018ict35@vau.jfn.ac.lk>
2024-06-16 09:36:13 +0530

    update: added gitignore to secure database credentials

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb+srv://harishscc123:S97r3Gdja1JA60PH@forms.ejlqcyb.mongodb.net/?retryWrites=true&w=majority&appName=Forms")
+load_dotenv()
 
+MONGO_DETAILS = os.getenv("MONGO_DETAILS")
 client = AsyncIOMotorClient(MONGO_DETAILS)
-
 database = client.typeform
 forms_collection = database.get_collection("forms")
 
